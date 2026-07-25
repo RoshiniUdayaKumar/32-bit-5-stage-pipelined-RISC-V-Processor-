dd552b9d5a5a237871161ef4fd79c1958d0b367b
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2026-07-25 17:03:45 +0530

    Update alu.v

--- ALU/alu.v
+++ ALU/alu.v
@@ -19,29 +19,37 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`define ADD 4'b0000
+`define SUB 4'b0001
+`define AND 4'b0010
+`define OR 4'b0011
+`define XOR 4'b0100
+`define SLL 4'b0101
+`define SRL 4'b0110
+`define SRA 4'b0111
 
 module ALU(
     input [31:0] BusA,
     input [31:0] BusB,
     input [3:0] ALUCtrl,
     output zero,
-    output reg [31:0] BusW
+    output reg [31:0] ALUResult
     );
     
     //logic for ALU 
     always@(*) begin 
     case(ALUCtrl) 
-    4'b0000: BusW= BusA +BusB;  //ADD operation 
-    4'b0001: BusW= BusA -BusB;  //SUB operation
-    4'b0010: BusW= BusA &BusB; // AND operation 
-    4'b0011: BusW= BusA|BusB; //OR operation
-    4'b0100: BusW= ~(BusA|BusB);  //NOR operation
-    4'b0101: BusW= BusA<<BusB;  //logical left shift operation
-    4'b0110: BusW= BusA>>BusB; // Logical right shift operation 
-    4'b0111: BusW= BusA>>>BusB; //Shift right arithmetic
-    default: BusW= 32'd0;
+    `ADD: ALUResult= BusA +BusB;  //ADD operation 
+    `SUB: ALUResult= BusA -BusB;  //SUB operation
+    `AND: ALUResult= BusA &BusB; // AND operation 
+    `OR: ALUResult= BusA|BusB; //OR operation
+    `XOR: ALUResult= BusA^BusB;  //XOR operation
+    `SLL: ALUResult= BusA<<BusB;  //logical left shift operation
+    `SRL: ALUResult= BusA>>BusB; // Logical right shift operation 
+    `SRA: ALUResult= BusA>>>BusB; //Shift right arithmetic
+    default: ALUResult= 32'd0;
     endcase
     end
     //logic for zero 
-    assign zero = (BusW==0)?1'b1:1'b0;
+    assign zero = (ALUResult==0)?1'b1:1'b0;
 endmodule
