3631376f05f57129575a8697a39226d97ae66675
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2026-07-25 17:01:30 +0530

    Update alu_control_unit.v

--- ALU_Control_Unit/alu_control_unit.v
+++ ALU_Control_Unit/alu_control_unit.v
@@ -19,50 +19,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-//function codes
-`define ADDFunc  6'b000000
-`define SUBFunc  6'b000010
-`define ANDFunc  6'b000011
-`define ORFunc   6'b100000
-`define NORFunc  6'b100001
-`define SLLFunc  6'b000001
-`define SRLFunc  6'b000110
-`define SRAFunc  6'b000111
-
 //ALUCtrl codes 
 `define ADD 4'b0000
 `define SUB 4'b0001
 `define AND 4'b0010
 `define OR 4'b0011
-`define NOR 4'b0100
+`define XOR 4'b0100
 `define SLL 4'b0101
 `define SRL 4'b0110
 `define SRA 4'b0111
+`define SLT 4'b1000
 
 module alu_control_unit(
-    input [3:0] ALUOp,
-    input [5:0] FunCode,
+    input [1:0] ALUOp,
+    input [2:0] funct3,
+    input [6:0] funct7,
     output reg [3:0] ALUCtrl
     );
     
- //logic 
- always@(*) begin 
- if(ALUOp!=4'b1111) 
- ALUCtrl=ALUOp;
- else if(ALUOp==4'b1111);
- case(FunCode)
- `ADDFunc: ALUCtrl=`ADD;
- `SUBFunc: ALUCtrl=`SUB;
- `ANDFunc: ALUCtrl=`AND;
- `ORFunc: ALUCtrl=`OR;
- `NORFunc: ALUCtrl= `NOR;
- `SLLFunc: ALUCtrl=`SLL;
- `SRLFunc: ALUCtrl=`SRL;
- `SRAFunc: ALUCtrl=`SRA;
- default: ALUCtrl=4'd0;
- endcase
- 
- end
-    
-    
+always @(*) begin 
+case(ALUOp) 
+2'b00: ALUCtrl = `ADD;  //for LW, SW, jump ins
+2'b01: ALUCtrl =`SUB; //for brnach ins
+2'b10: begin   //for r-type ins
+case (funct3)
+3'b000: ALUCtrl = (funct7[5]) ? `SUB:`ADD;
+3'b001: ALUCtrl =`SLL;
+3'b010: ALUCtrl=`SLT;
+3'b100: ALUCtrl=`XOR;
+3'b101: ALUCtrl= (funct7[5])? `SRA:`SRL;
+3'b110: ALUCtrl= `OR;
+3'b111: ALUCtrl=`AND;
+default: ALUCtrl = 4'd0;
+endcase
+end
+default: ALUCtrl=4'd0;
+endcase
+end     
 endmodule
