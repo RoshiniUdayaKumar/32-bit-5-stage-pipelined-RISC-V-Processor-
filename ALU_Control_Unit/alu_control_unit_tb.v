30492e09b6368a312d90743014fa48add456e7d2
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2026-07-25 17:01:58 +0530

    Update alu_control_unit_tb.v

--- ALU_Control_Unit/alu_control_unit_tb.v
+++ ALU_Control_Unit/alu_control_unit_tb.v
@@ -19,64 +19,61 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-//function codes
-`define ADDFunc  6'b000000
-`define SUBFunc  6'b000010
-`define ANDFunc  6'b000011
-`define ORFunc   6'b100000
-`define NORFunc  6'b100001
-`define SLLFunc  6'b000001
-`define SRLFunc  6'b000110
-`define SRAFunc  6'b000111
-
 //ALUCtrl codes 
 `define ADD 4'b0000
 `define SUB 4'b0001
 `define AND 4'b0010
 `define OR 4'b0011
-`define NOR 4'b0100
+`define XOR 4'b0100
 `define SLL 4'b0101
 `define SRL 4'b0110
 `define SRA 4'b0111
+`define SLT 4'b1000
 
 module alu_control_unit_tb();
 //port declaration 
-reg [3:0] ALUOp;
-reg [5:0] FunCode;
+reg [1:0] ALUOp;
+reg [2:0] funct3;
+reg [6:0] funct7;
 wire [3:0] ALUCtrl;
 //module declaration 
-alu_control_unit alu_control_unit_dut(ALUOp, FunCode, ALUCtrl);
+alu_control_unit alu_control_unit_dut(ALUOp, funct3, funct7, ALUCtrl);
 //stimulus 
 initial begin 
 ALUOp= 4'b0000;
-FunCode=`NORFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`ADDFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`ORFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`SUBFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`ANDFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`NORFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`SLLFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`SRLFunc;
-
-#20 ALUOp=4'b1111;
-FunCode =`SRAFunc;
 
 
+#20 ALUOp=2'b00;  //add operation 
+
+#20 ALUOp=2'b01;  //branch operation
+
+#20 ALUOp=2'b11;  //default value
+funct3= 3'b000 ;
+funct7[5] =1;  
+
+#20 ALUOp=2'b10;  //sub operation
+funct3= 3'b000 ;
+funct7[5] =1; 
+
+#20 ALUOp=2'b10;  //add operation
+funct3= 3'b000 ;
+funct7[5] =0; 
+
+#20 ALUOp=2'b10; //sll
+funct3= 3'b001;
+
+#20 ALUOp=2'b10; //default
+funct3= 3'b011 ;
+
+#20 ALUOp=2'b10;
+funct3= 3'b101;   //sra
+funct7[5] =1;
+
+#20 ALUOp=2'b10;
+funct3= 3'b101;   //srl
+funct7[5] =0;
+
+#20 ALUOp=2'b11; //default
 
 end
 endmodule
